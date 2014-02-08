cb51b4a4beedf3e0de1116298aa06e509e5a7b6f
Ted Meyer <tmathmeyer@gmail.com>
2014-02-08 17:30:47 -0500

    fix?

--- assignment_2/part_2/get_process_info.c
+++ assignment_2/part_2/get_process_info.c
@@ -25,7 +25,7 @@
 
 int main(int argc, char** argv)
 {
-	prinfo* cpi;
+	prinfo* cpi = (prinfo*)(malloc(sizeof(prinfo)));
 	printf("syscall result: %i\n", syscall(__NR_cs3013_syscall1, cpi));
 
 	printf("╔═══════════════════════╗\n");
