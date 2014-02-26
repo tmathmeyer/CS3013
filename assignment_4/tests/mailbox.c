87b0b0f4f53c6d9f4f1be2e1a6545e0f0422ead9
Ted Meyer <tmathmeyer@gmail.com>
2014-02-26 12:04:55 -0500

    added tests:

---
+++ assignment_4/tests/mailbox.c
@@ -0,0 +1,46 @@
+/*
+ * Adapted from CS-502 Project #3, Fall 2006
+ *	originally submitted by Cliff Lindsay
+ * Modified for CS-502, Summer 2011 by Alfredo Porras
+ * Adapted for CS-502 at Cisco Systems, Fall 2011
+ * 
+ * Modified Ted Meyer for CS3013
+ *   2014-02-26
+ *
+ * License: GPLv2
+ */
+
+#include "mailbox.h"
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#define __NR_mailbox_send	349
+#define __NR_mailbox_rcv	350
+#define __NR_mailbox_manage	351
+
+/*
+ * Functions for sending messages
+ * 
+ */
+int SendMsg(pid_t dest, void *msg, int len, bool block)
+{
+    return syscall(__NR_mailbox_send, dest, msg, len, block);
+}
+
+/*
+ * function for recieving messages
+ */
+int RcvMsg(pid_t *sender, void *msg, int *len, bool block)
+{
+    return syscall(__NR_mailbox_rcv, sender, msg, len, block);
+}
+
+/*
+ * functions for maintaining mailboxes
+ * 
+ */
+int ManageMailbox(bool stop, int *count)
+{
+    return syscall(__NR_mailbox_manage, stop, count);
+}
+
