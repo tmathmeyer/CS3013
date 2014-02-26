b508b81ef632cf874b7c3aaea51acd393245b7e1
Ted Meyer <tmathmeyer@gmail.com>
2014-02-25 23:37:02 -0500

    updated test

--- assignment_4/test.c
+++ assignment_4/test.c
@@ -20,6 +20,9 @@ int main()
 	printf("syscall result: %ld\n", syscall(SEND, getpid(), (void*)voidstar, 10, false));
 
 
-	printf("syscall create: %ld\n", syscall(MANG, 0, &msgs));
-	printf("there were %i messages\n", msgs);
+	pid_t sender;
+	void* message;
+	int length;
+	printf("syscall create: %ld\n", syscall(RECV, &sender, message, &length));
+	printf("the message was %i chars long", length);
 }
