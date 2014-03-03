6ccf64045aeee1b722494a56d6814f3c8032d15a
Ted Meyer <tmathmeyer@gmail.com>
2014-03-03 00:55:02 -0500

    bf55

--- assignment_4/tests/testmailbox7.c
+++ assignment_4/tests/testmailbox7.c
@@ -15,44 +15,50 @@
 
 #define CHILD_NUM 50
 
-int main() {
-  int childCounter;
-  
-  // fork enough children so that they can all send a message
-  // to the parent and hold a pointer to it's mailbox
-  for(childCounter = 0; childCounter < CHILD_NUM; childCounter++) {
-    int childPID = fork();
+int main()
+{
+    int childCounter;
     
-    if(childPID == 0){
-      pid_t sender;
-      void *msg[128];
-      int len;
-      bool block = true;
-      
-      RcvMsg(&sender,msg,&len,block);
+    // fork enough children so that they can all send a message
+    // to the parent and hold a pointer to it's mailbox
+    for(childCounter = 0; childCounter < CHILD_NUM; childCounter++)
+    {
+        int childPID = fork();
+        
+        if(childPID == 0
+        {
+            pid_t sender;
+            void *msg[128];
+            int len;
+            bool block = true;
+            
+            RcvMsg(&sender,msg,&len,block);
+        
+            
+            printf("Message: %s\n", (char *)msg);
+            char myMesg[] = "I am your child";
+            if(SendMsg(sender, myMesg, 16, block))
+            {
+                printf("Child send failed.\n");
+            }
+            
+            return 0;
+        }
+        else
+        {
+            char mesg[] = "I am your father";
+            if (SendMsg(childPID, mesg, 17, false))
+            {
+                printf("Send failed\n");
+            }
+        }
+    }
     
-      
-      printf("Message: %s\n", (char *)msg);
-      char myMesg[] = "I am your child";
-      if(SendMsg(sender, myMesg, 16, block)) {
-	printf("Child send failed.\n");
-      }
-      
-      return 0;
-    }
-    else{
-      char mesg[] = "I am your father";
-      if (SendMsg(childPID, mesg, 17, false)){
-	printf("Send failed\n");
-      }
-    }
-  }
-  
-  // the parent sleeps for a little time
-  // waiting for some of it's children to get a pointer
-  // to its mailbox
-  // before trying to kill its own process.
-  usleep(1000);
-  printf("Parent dies.\n");
-  return 0;
+    // the parent sleeps for a little time
+    // waiting for some of it's children to get a pointer
+    // to its mailbox
+    // before trying to kill its own process.
+    usleep(1000);
+    printf("Parent dies.\n");
+    return 0;
 }
