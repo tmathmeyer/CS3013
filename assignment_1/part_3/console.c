4ce9c035ed7af29410c8da823f9136e2793f2ce8
Ted Meyer <tmathmeyer@gmail.com>
2014-09-05 17:19:23 -0400

    fixed indentation

--- assignment_1/part_3/console.c
+++ assignment_1/part_3/console.c
@@ -10,116 +10,116 @@
 
 char** split_args(char* buffer)
 {
-	char* clone = (char*)(malloc(128));
-	int   count = 0;
-	strncpy(clone, buffer, 128);
+    char* clone = (char*)(malloc(128));
+    int   count = 0;
+    strncpy(clone, buffer, 128);
 
-	char* toks = strtok (clone," ");
-	while (toks != NULL)
-	{
-		count++;
-		toks = strtok (NULL, " ");
-	}
-	free(toks);
+    char* toks = strtok (clone," ");
+    while (toks != NULL)
+    {
+        count++;
+        toks = strtok (NULL, " ");
+    }
+    free(toks);
 
-	char** result = (char**)(malloc(sizeof(char*) * count));
-	char** writer = result;
+    char** result = (char**)(malloc(sizeof(char*) * count));
+    char** writer = result;
 
-	toks = strtok (buffer," ");
-	while (toks != NULL)
-	{
-		char* arg = (char*)(malloc(sizeof(char) * (strlen(toks) + 1)));
-		strcpy(arg, toks);
-		strcat(arg, "\0");
-		*(writer++) = arg;
-		toks = strtok (NULL, " ");
-	}
-	free(toks);
+    toks = strtok (buffer," ");
+    while (toks != NULL)
+    {
+        char* arg = (char*)(malloc(sizeof(char) * (strlen(toks) + 1)));
+        strcpy(arg, toks);
+        strcat(arg, "\0");
+        *(writer++) = arg;
+        toks = strtok (NULL, " ");
+    }
+    free(toks);
 
-	return result;
+    return result;
 }
 
 int change_dir(char* dirname)
 {
-	char dir[1024];
-	if (dirname)
-	{
-		chdir(dirname);
-	}
-	else
-	{
-		chdir("/");
-	}
+    char dir[1024];
+    if (dirname)
+    {
+        chdir(dirname);
+    }
+    else
+    {
+        chdir("/");
+    }
 
-	if (getcwd(dir, sizeof(dir)) != NULL)
-	{
-		printf("%s\n", dir);
-	}
+    if (getcwd(dir, sizeof(dir)) != NULL)
+    {
+        printf("%s\n", dir);
+    }
 }
 
 int exeunt = 0;
 
 void* listen_all(void* arg)
 {
-	proc_list** running = (proc_list**)(arg);
-	while(!*running && exeunt == 0)
-	{
-		while(*running)
-		{
-			*running = prune(*running);
-		}
-	}
-	
+    proc_list** running = (proc_list**)(arg);
+    while(!*running && exeunt == 0)
+    {
+        while(*running)
+        {
+            *running = prune(*running);
+        }
+    }
+    
 }
 
 int main(int argc, char*argv[])
 {
-	printf("~~$ ");
-	char* buffer = (char*)malloc(1024);
-	int bufferspace = 0;
-	proc_list* ptr = 0;
-	proc_list** running = &ptr;
-	*running = 0;
-	int background = 0;
+    printf("~~$ ");
+    char* buffer = (char*)malloc(1024);
+    int bufferspace = 0;
+    proc_list* ptr = 0;
+    proc_list** running = &ptr;
+    *running = 0;
+    int background = 0;
 
-	pthread_t thread;
-	pthread_create(&thread, NULL, listen_all, running);
+    pthread_t thread;
+    pthread_create(&thread, NULL, listen_all, running);
 
 
-	while(exeunt == 0)
-	{
-		char c = getchar();
-		if (c == '\n')
-		{
-			if (bufferspace != 0)
+    while(exeunt == 0)
+    {
+        char c = getchar();
+        if (c == '\n')
+        {
+            if (bufferspace != 0)
             {
                 char** args = split_args(buffer);
 
-			    if (strcmp("exit", args[0]) == 0)
-			    {
-			    	exeunt = 1;
-			    }
-			    else if (strcmp("cd", args[0]) == 0)
-			    {
-			    	change_dir(args[1]);
-		    	}
-		    	else
-		    	{
-		    		
-		    		if (background)
-		    		{
-		    			*running = exec(args, running[0]);
-		    			printf("\n~~$ ");
-		    		}
-		    		else
-		    		{
-		    			proc_info* start = get_init();
-		    			print_info(exec_forg(args, start));
-		    		}
-		    	}
+                if (strcmp("exit", args[0]) == 0)
+                {
+                    exeunt = 1;
+                }
+                else if (strcmp("cd", args[0]) == 0)
+                {
+                    change_dir(args[1]);
+                }
+                else
+                {
+                    
+                    if (background)
+                    {
+                        *running = exec(args, running[0]);
+                        printf("\n~~$ ");
+                    }
+                    else
+                    {
+                        proc_info* start = get_init();
+                        print_info(exec_forg(args, start));
+                    }
+                }
 
-		    	memset(buffer, 0, 128);
-			    bufferspace = 0;
+                memset(buffer, 0, 128);
+                bufferspace = 0;
             }
             else
             {
@@ -131,18 +131,18 @@ int main(int argc, char*argv[])
             exeunt = 1;
             printf("\n");
         }
-		else if (c != '\b')
-		{
-			background = (c=='&');
-			if (!background)
-			{
-				buffer[(bufferspace++)%1024] = c;
-			}
-		}
-	}
+        else if (c != '\b')
+        {
+            background = (c=='&');
+            if (!background)
+            {
+                buffer[(bufferspace++)%1024] = c;
+            }
+        }
+    }
 
-	printf("waiting for termination from all sub-processess");
-	while(*running);
+    printf("waiting for termination from all sub-processess");
+    while(*running);
 
-	return 0;
+    return 0;
 }
